e6f9bc9491ca64a0d4490c9eb29cc2e2371f544f
Tim Berry <timothyaberry@gmail.com>
2015-10-20 09:09:57 -0600

    First problem completed

--- practice.js
+++ practice.js
@@ -23,17 +23,21 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+var user = {
+  username: "tberry",
+  email: "timothyaberry@gmail.com",
+  getUsername: function() {
+    return this.username;
+  }
+};
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
+user.getUsername();
 
-//Next Problem
 
 
 // Write the function definitions which will make the following function invocations function properly.
-
-  //Function Invocations Here
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -42,6 +46,7 @@ prius.moveCar(); //increments prius' move property by 10. Returns the new move p
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
 //Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the write object (prius vs mustang).
+
 
 
 
